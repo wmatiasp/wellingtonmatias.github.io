d51975939851b23827ce4733cf89d010b3838a40
wmatiasp <wmatiasp@gmail.com>
2025-06-13 16:19:23 -0300

    Add files via upload

---
+++ case-dp6/case/js/tagueamento.js
@@ -0,0 +1,2 @@
+// Toda configuração foi feita através do Google Tag Manager junto com os screenshots de validação
+// Os arquivos foram disponibilizados no Github: https://github.com/wmatiasp/dp6teste
